8e805bb7ac914ceb1b89f7a79d0b9091c166cb79
qiuping01 <2187718660@qq.com>
2025-07-20 21:31:22 +0800

    IoC创建对象

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -6,7 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-        Student student = new Student();
-        System.out.println(student);
+        //1、加载spring.xml
+        ApplicationContext ioc =
+                new ClassPathXmlApplicationContext("spring.xml");
+        //2、通过id获取对象
+        Student student2 = (Student) ioc.getBean("student2");
+        System.out.println(student2);
     }
 }
