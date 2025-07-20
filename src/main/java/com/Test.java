d54568e753afd46d5e0ec987f7f212235e044060
qiuping01 <2187718660@qq.com>
2025-07-20 21:48:18 +0800

    反射复习

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -4,13 +4,22 @@ import com.test.entity.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.lang.reflect.Constructor;
+
+
 public class Test {
     public static void main(String[] args) {
-        //1、加载spring.xml
-        ApplicationContext ioc =
-                new ClassPathXmlApplicationContext("spring.xml");
-        //2、通过id获取对象
-        Student student2 = (Student) ioc.getBean("student2");
-        System.out.println(student2);
+        test("com.test.entity.Student");
+    }
+
+    // 反射
+    public static void test(String string) {
+        try {
+            Class<?> aClass = Class.forName(string);
+            Constructor<?> constructor = aClass.getConstructor(null);
+            System.out.println(constructor.newInstance(null));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
